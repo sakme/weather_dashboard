b0c28d064c3641137990c24303f4941b4e5bbabb
sakme <alan@akme.us>
2021-12-01 12:09:22 -0500

    search and history functions added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+var userFormEl = document.querySelector("#searchForm");
+var userFormHistoryEl = document.querySelector("#search_history");
+var locationInputEl = document.querySelector("#searchInput");
 var apiKey = "&appid=38b9f7071f9eafef52b0970c1e55a172";
 var baseURL = "http://api.openweathermap.org/data/2.5/";
 var oneCall = "onecall?";
@@ -6,10 +9,16 @@ var cCode = ",us";
 var units = "&units=imperial";
 var latPar = "lat=";
 var lonPar = "&lon=";
-var exclude = "&exclude=hourly,minutely";
+var exclude = "&exclude=hourly,minutely,alerts";
+var city = "";
+var state = "";
+var searchHistory = [];
 
 var getLocation = function(local) {
-    fetch(baseURL + cityCall + local + cCode + units + apiKey)
+    city = local.split(", ")[0];
+    state = local.split(", ")[1];
+    
+    fetch(baseURL + cityCall + city + "," + state + cCode + units + apiKey)
     .then(function(response) {
         if (response.ok) {
             response.json().then(function(locationData) {
@@ -17,7 +26,10 @@ var getLocation = function(local) {
                 var lon = locationData.coord.lon;
                 getWeather(lat,lon);
             })
-        }
+        } 
+        // else {
+        //     document.location.reload()
+        // }
     });
 };
 
@@ -26,10 +38,57 @@ var getWeather = function(lat,lon) {
     .then(function(response) {
         if (response.ok) {
             response.json().then(function(data) {
-                console.log(data);
+                return(data);
             })
         }
     })
 }
 
-getLocation("columbus,oh");
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+
+    var local = locationInputEl.value.trim();
+
+    locationInputEl.value = "";
+
+    var button = document.createElement("button");
+    button.classList = "col-12 searchHistoryButton";
+    button.setAttribute("id", "searchHistoryButton");
+    button.setAttribute("type", "submit");
+    button.setAttribute("form", "searchHistoryButton");
+    button.setAttribute("value", local);
+    button.textContent = local;
+
+    var form = document.querySelector("#search_history");
+    
+    form.appendChild(button);
+
+    searchHistory.push(local);
+
+    if (local) {
+        getLocation(local);
+    } else {
+        alert("Please enter a valid location.");
+    }
+};
+
+var formHistorySubmitHandler = function(event) {
+    // event.preventDefault();
+
+    // console.log(event.target.outerText);
+
+    var locale = event.target.outerText;
+
+    console.log(locale);
+
+    searchHistory.push(locale);
+
+    if (locale) {
+        getLocation(locale);
+    } else {
+        alert("Please enter a valid location.");
+    }
+};
+
+userFormEl.addEventListener("submit", formSubmitHandler);
+userFormHistoryEl.addEventListener("click", formHistorySubmitHandler);
